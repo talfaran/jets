8325e043387cfcf13e60972ed18d031b9210ee82
Talfaran <tal1faran@gmail.com>
2018-05-13 18:02:18 +0300

    launch rocket!

--- rocket.js
+++ rocket.js
@@ -1,8 +1,12 @@
-class Rocket {
-    constructor(color, size){
-      this.color = color;
-      this.size = size;
-    }
+class Rocket{
+  constructor(color, size) {
+    this.color = color;
+    this.size = size;
   }
   
-  module.exports = Rocket;
+  launch () {
+    console.log('3, 2, 1, blastoff!');
+  }
+}
+
+module.exports = Rocket;
